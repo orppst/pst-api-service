bb85ac7fae1b22f2c95e553f7ba27ecd07a6f774
Darren Walker <darrenwalker1@hotmail.co.uk>
2022-11-30 15:09:32 +0000

    first attempt using kakfa to communicate ProposalModel

--- src/main/java/org/orph2020/pst/apiimpl/rest/ProposalResource.java
+++ src/main/java/org/orph2020/pst/apiimpl/rest/ProposalResource.java
@@ -3,16 +3,25 @@ package org.orph2020.pst.apiimpl.rest;
  * Created on 16/03/2022 by Paul Harrison (paul.harrison@manchester.ac.uk).
  */
 
+import io.smallrye.mutiny.Multi;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.ivoa.dm.proposal.prop.EmerlinExample;
 import org.ivoa.dm.proposal.prop.ProposalModel;
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.reactive.RestStreamElementType;
+
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -27,17 +36,34 @@ public class ProposalResource {
       this.logger = logger;
    }
 
+   @Channel("query-proposal")
+   Emitter<String> queryProposalEmitter;
+
+   @POST
+   @Path("/query")
+   @Produces(MediaType.TEXT_PLAIN)
+   public String createQuery() {
+      String q = "SELECT o FROM ObservingProposal o ORDER BY o.code";
+      queryProposalEmitter.send(q);
+      return q;
+   }
+
+   /*
+      the return type could be something other than Multi
+    */
+
+   @Channel("proposals")
+   Multi<String> proposals;
+
    @GET
    @Operation(summary = "Returns all the proposals in the database")
    @APIResponse(
-         responseCode = "200",
-         description = "Gets all proposals"
+           responseCode = "200",
+           description = "Gets all proposals"
    )
-   public ProposalModel getAllProposals() {
-      ProposalModel proposalModel = new ProposalModel();
-      proposalModel.addContent(new EmerlinExample().getProposal()); //FIXME this needs to really contact the propdm-sync-service via kafka
-      return proposalModel ;
-
+   @Produces(MediaType.SERVER_SENT_EVENTS) // to trigger dynamic changes in html
+   public Multi<String> getAllProposals() {
+      return proposals.log();
    }
 
 }
