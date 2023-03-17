afedeb731b3d4870adf0e84773de4ce58f9b5c12
Paul Harrison <paul.harrison@manchester.ac.uk>
2023-03-17 20:20:07 +0000

    update the get observatory to return the db key and a name that can be used as description
    idea is to show a pattern for the api such that
    
    /pst/api/objects
    
    returns a list of database ids and a string that can be used to describe the object in the GUI rather than
    returning the whole of the object.

--- src/main/java/org/orph2020/pst/apiimpl/rest/ObservatoryResource.java
+++ src/main/java/org/orph2020/pst/apiimpl/rest/ObservatoryResource.java
@@ -6,23 +6,24 @@ package org.orph2020.pst.apiimpl.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
-import org.ivoa.dm.proposal.prop.Observatory;
-import org.ivoa.dm.proposal.prop.Organization;
+import org.orph2020.pst.common.json.ObjectIdentifier;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("observatories")
 @Tag(name = "proposal-tool")
 public class ObservatoryResource {
+
    @PersistenceContext
    EntityManager em;  // exists for the application lifetime no need to close
 
@@ -30,10 +31,17 @@ public class ObservatoryResource {
    ObjectMapper mapper;
    @GET
     @Operation(summary = "Get all of the Observatories")
-    public List<Observatory> getObservatories(){
-       String queryStr = "SELECT o FROM Observatory o ORDER BY o.name";
-       TypedQuery<Observatory> query = em.createQuery(queryStr, Observatory.class);
-       return query.getResultList();
+    public List<ObjectIdentifier> getObservatories(){
+       List<ObjectIdentifier> retval = new ArrayList<>();
+       String queryStr = "SELECT  o._id,o.name FROM Observatory o ORDER BY o.name";
+       Query query = em.createQuery(queryStr);
+       List<Object[]> results = query.getResultList();
+       for (Object[] r : results)
+       {
+          retval.add(new ObjectIdentifier((Long) r[0], (String) r[1]));
+       }
+
+      return retval;
     }
 
 
