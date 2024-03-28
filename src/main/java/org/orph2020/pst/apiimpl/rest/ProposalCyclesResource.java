be2310e798eb9dabf4f8a3324d01726c7531816e
Darren Walker <darrenwalker1@hotmail.co.uk>
2024-03-28 16:03:11 +0000

    code tidy, add post, put, and delete methods for available resources in a proposal cycle

--- src/main/java/org/orph2020/pst/apiimpl/rest/ProposalCyclesResource.java
+++ src/main/java/org/orph2020/pst/apiimpl/rest/ProposalCyclesResource.java
@@ -6,18 +6,13 @@ package org.orph2020.pst.apiimpl.rest;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.ivoa.dm.proposal.management.*;
-import org.ivoa.dm.proposal.prop.ObservingProposal;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.reactive.RestQuery;
 import org.orph2020.pst.common.json.ObjectIdentifier;
 import org.orph2020.pst.common.json.ProposalCycleDates;
-import org.orph2020.pst.common.json.ProposalSynopsis;
 
-import jakarta.persistence.TypedQuery;
-import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import java.util.*;
 
 @Path("proposalCycles")
