a881f8db6f4cfbaf339da2b2c98023c577ee1b36
Darren Walker <darrenwalker1@hotmail.co.uk>
2024-09-26 11:11:20 +0100

    add call to delete uploaded files on request, ensure file upload names are unique per Justification, add convenience class tag name, add checks of the 'which' rest path parameter.

--- src/main/java/org/orph2020/pst/apiimpl/rest/JustificationsResource.java
+++ src/main/java/org/orph2020/pst/apiimpl/rest/JustificationsResource.java
@@ -4,12 +4,20 @@ package org.orph2020.pst.apiimpl.rest;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.apache.commons.io.FilenameUtils;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.ivoa.dm.proposal.prop.Justification;
 import org.ivoa.dm.proposal.prop.ObservingProposal;
 import org.ivoa.dm.proposal.prop.TextFormats;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Objects;
@@ -24,6 +32,7 @@ import java.util.Objects;
  */
 
 @Path("proposals/{proposalCode}/justifications")
+@Tag(name = "proposals-justifications")
 public class JustificationsResource extends ObjectResourceBase {
 
     @ConfigProperty(name= "supporting-documents.store-root")
@@ -171,7 +180,7 @@ public class JustificationsResource extends ObjectResourceBase {
 
         if (persisted.getFormat() == TextFormats.LATEX) {
             try {
-                //create and write the string to file if it is latex format
+                //create and write the string to file
                 writeLatexToFile(persisted.getText(), proposalCode, which);
             } catch (IOException e) {
                 //if we can't write the justification text to the *.tex file then we should roll back
@@ -180,8 +189,126 @@ public class JustificationsResource extends ObjectResourceBase {
                 throw new WebApplicationException(e.getMessage());
             }
         }
-
-
         return persisted;
     }
+
+    /**
+     * Function to get the file identified by the parameters
+     * @param proposalCode the proposal id (determined by rest end-point path)
+     * @param which 'scientific' or 'technical' (determined by rest end-point path)
+     * @param filename the name of the file (file upload name or user supplied)
+     * @return the File object given the parameters above
+     */
+    private File getFile(Long proposalCode, String which, String filename) {
+        return new File(
+                documentStoreRoot
+                        + "/proposals/"
+                        + proposalCode
+                        + "/justifications/"
+                        + which,
+                filename
+        );
+    }
+
+    //required to make form-upload input work
+    @Schema(type = SchemaType.STRING, format = "binary")
+    public static class UploadItemSchema {}
+
+    //non-transactional, no modification to the database occurs
+    @POST
+    @Path("{which}/fileUpload")
+    @Operation(summary = "upload files for latex Justifications, only *.bib, *.jpg, and *.png supported")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadLatexFiles(@PathParam("proposalCode") Long proposalCode,
+                                     @PathParam("which") String which,
+                                     @RestForm("document") @Schema(implementation = UploadItemSchema.class)
+                                         FileUpload fileUpload)
+        throws WebApplicationException
+    {
+        if (!which.equals("technical") && !which.equals("scientific")) {
+            throw new WebApplicationException(
+                    String.format("Justifications are either 'scientific' or 'technical'; I got %s", which)
+            );
+        }
+
+        if (fileUpload == null) {
+            throw new WebApplicationException("No file uploaded");
+        }
+
+        String contentType = fileUpload.contentType();
+        if (contentType == null) {
+            throw new WebApplicationException("No content type information available");
+        }
+
+        String extension = FilenameUtils.getExtension(fileUpload.fileName());
+        if (extension == null || extension.isEmpty()) {
+            throw new WebApplicationException("Uploads require the correct file extension");
+        }
+
+        switch (contentType) {
+            //.bib, .png, or .jpg (.jpeg) only; notice .bib may have 'application/octet-stream' type
+            case "application/octet-stream":
+            case "application/x-bibtex":
+            case "image/jpeg":
+            case "image/png":
+                if (!extension.equals("bib") && !extension.equals("jpeg") &&
+                        !extension.equals("jpg") && !extension.equals("png"))
+                    throw new WebApplicationException("Invalid file extension");
+                break;
+            default:
+                throw new WebApplicationException(
+                        String.format("content-type: %s is not supported", contentType));
+        }
+
+        //NOTICE: content-type and file extension can be manipulated so that they misrepresent the
+        // actual contents of the file. However, on Linux and Linux-like systems uploaded files saved
+        // in this way have read-write permissions for the 'user' only (the "application" is the user here),
+        // i.e. no executable permissions, and no permissions for 'groups' or 'other users'.
+
+        String filename = fileUpload.fileName();
+
+        File destination = getFile(proposalCode, which, filename);
+
+        //check filename is unique per Justification
+        if (destination.exists()) {
+            throw new WebApplicationException(
+                    String.format("File %s already exists: Please use unique names for uploads",
+                            filename));
+        }
+
+        //save the uploaded file to the assigned location
+        if (!fileUpload.uploadedFile().toFile().renameTo(destination)) {
+            throw new WebApplicationException("Unable to save uploaded file");
+        }
+
+        return Response.ok(String.format("File %s saved", filename)).build();
+    }
+
+    //non-transactional, no modification to the database occurs
+    @DELETE
+    @Path("{which}/fileUpload")
+    @Operation(summary = "delete the given file from the Justification document store")
+    public Response removeLaTexFile(@PathParam("proposalCode") Long proposalCode,
+                                    @PathParam("which") String which,
+                                    String filename)
+    throws WebApplicationException
+    {
+        if (!which.equals("technical") && !which.equals("scientific")) {
+            throw new WebApplicationException(
+                    String.format("Justifications are either 'scientific' or 'technical'; I got %s", which)
+            );
+        }
+
+        File toDelete = getFile(proposalCode, which, filename);
+        if (toDelete.exists()) {
+            if (toDelete.delete()) {
+                return Response.ok(String.format("File %s deleted", filename)).build();
+            } else {
+                throw new WebApplicationException(String.format("Unable to delete file: %s", filename));
+            }
+        } else {
+            throw new WebApplicationException(String.format("Nonexistent file: %s", filename));
+        }
+    }
+
 }
