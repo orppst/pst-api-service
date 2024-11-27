08c9d5bf8ff341d5779c716c8b3f5b586fe80b63
Allan England <allan.england@manchester.ac.uk>
2024-11-27 13:31:39 +0000

    Populate some more details

--- src/main/java/org/orph2020/pst/apiimpl/rest/UserProposalsSubmitted.java
+++ src/main/java/org/orph2020/pst/apiimpl/rest/UserProposalsSubmitted.java
@@ -9,8 +9,10 @@ import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.ivoa.dm.proposal.management.AllocatedProposal;
 import org.ivoa.dm.proposal.management.SubmittedProposal;
 import org.ivoa.dm.proposal.prop.ProposalKind;
+import org.ivoa.dm.proposal.prop.RelatedProposal;
 import org.orph2020.pst.common.json.ObjectIdentifier;
 import org.orph2020.pst.common.json.SubmittedProposalSynopsis;
 
@@ -28,27 +30,37 @@ public class UserProposalsSubmitted extends ObjectResourceBase {
 
     @GET
     @Operation(summary = "Get a list of synopsis for proposals submitted by the authenticated user")
-    @RolesAllowed("default-roles-orppst")
+    //@RolesAllowed("default-roles-orppst")
     public List<SubmittedProposalSynopsis> getProposalsSubmitted()
     {
-        long personId = subjectMapResource.subjectMap(userInfo.getSubject()).getPerson().getId();
+        long personId = 4L;//subjectMapResource.subjectMap(userInfo.getSubject()).getPerson().getId();
         List<SubmittedProposalSynopsis> listOfSubmitted = new ArrayList<>();
 
-        String queryStr = "select distinct o._id,o.title,o.summary,o.kind " +
-                "from SubmittedProposal o, Investigator inv, Investigator i "
+        String queryStr1 = "select distinct o._id,o.title,o.summary,o.kind,c._id "
+                + "from SubmittedProposal o, ProposalCycle c "
+                + "where o member of c.submittedProposals";
+
+        String queryStr = "select distinct o._id,o.title,o.summary,o.kind,c._id "
+                + "from SubmittedProposal o, Investigator inv, Investigator i, ProposalCycle c "
                 + "where inv member of o.investigators and inv.person._id = " + personId + " "
-                + "and i member of o.investigators";
+                + "and i member of o.investigators "
+                + "and o member of c.submittedProposals";
 
         Query query = em.createQuery(queryStr);
         List<ObjectIdentifier[]> results = query.getResultList();
         for (Object[] r : results) {
+            SubmittedProposal prop = findObject(SubmittedProposal.class, (long)r[0]);
+            List<RelatedProposal> sourcePropList = prop.getRelatedProposals();
+            long sourcePropId = 0;
+            if(!sourcePropList.isEmpty())
+                sourcePropId = sourcePropList.get(0).getId();
             listOfSubmitted.add(new SubmittedProposalSynopsis(
-                    (long) r[0],            // db id
-                    (String) r[1],          // title
-                    (String) r[2],          // summary
-                    (ProposalKind) r[3],    // kind
-                    0,                      // FIXME: cycle id
-                    0,                      // FIXME: source proposal db id
+                    (long) r[0],         // db id
+                    (String) r[1],       // title
+                    (String) r[2],       // summary
+                    (ProposalKind) r[3], // kind
+                    sourcePropId,        // source proposal db id
+                    (long) r[4],         // cycle id
                     "Nothing yet"
             ));
         }
@@ -62,7 +74,8 @@ public class UserProposalsSubmitted extends ObjectResourceBase {
     @Path("{submittedProposalId}/withdraw")
     @RolesAllowed("default-roles-orppst")
     @Transactional(rollbackOn = {WebApplicationException.class})
-    public Response withdrawProposal(@PathParam("submittedProposalId") long submittedProposalId)
+    public Response withdrawProposal(@PathParam("submittedProposalId") long submittedProposalId,
+                                     @QueryParam("cycleId") long cycleCode)
         throws WebApplicationException
     {
         long personId = subjectMapResource.subjectMap(userInfo.getSubject()).getPerson().getId();
@@ -73,7 +86,9 @@ public class UserProposalsSubmitted extends ObjectResourceBase {
         //Check if the cycle deadline has passed
 
         //Withdraw from observing cycle & delete it
-        //submittedProposal.getObservations().forEach(observation -> em.remove(observation));
+        //AllocatedProposal allocatedProposal = findChildByQuery(AllocatedProposal.class, SubmittedProposal.class, "submitted_ID", cycleCode, submittedProposalId);
+
+        submittedProposal.getObservations().forEach(observation -> em.remove(observation));
         return removeObject(SubmittedProposal.class, submittedProposalId);
 
     }
