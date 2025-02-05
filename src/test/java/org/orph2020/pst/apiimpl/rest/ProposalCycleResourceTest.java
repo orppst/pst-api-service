fd2e6f2db0b28ab7a6202a38f35360c736a7d54f
Allan England <allan.england@manchester.ac.uk>
2025-02-05 11:15:47 +0000

    New test to add a proposal cycle

--- src/test/java/org/orph2020/pst/apiimpl/rest/ProposalCycleResourceTest.java
+++ src/test/java/org/orph2020/pst/apiimpl/rest/ProposalCycleResourceTest.java
@@ -3,20 +3,26 @@ package org.orph2020.pst.apiimpl.rest;
  * Created on 21/04/2023 by Paul Harrison (paul.harrison@manchester.ac.uk).
  */
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.internal.mapping.Jackson2Mapper;
+import org.ivoa.dm.proposal.management.ProposalCycle;
+import org.ivoa.dm.proposal.management.Observatory;
 import org.ivoa.dm.proposal.management.TAC;
-import org.ivoa.dm.proposal.prop.Person;
+import org.ivoa.dm.proposal.prop.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import jakarta.inject.Inject;
 
+import java.util.Date;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 import  static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static io.restassured.http.ContentType.JSON;
 
 @QuarkusTest
 public class ProposalCycleResourceTest {
@@ -31,6 +37,52 @@ public class ProposalCycleResourceTest {
          return mapper;
       }));
    }
+
+   @Test
+   void addProposalCycle() throws JsonProcessingException {
+      int observatoryId =
+              given()
+                      .when()
+                      .param("name","Jodrell Bank")
+                      .get("observatories")
+                      .then()
+                      .statusCode(200)
+                      .body(
+                              "$.size()", equalTo(1)
+                      )
+                      .extract().jsonPath().getInt("[0].dbid");
+
+      Observatory lookHere =
+              given()
+                      .when()
+                      .get("observatories/" + observatoryId)
+                      .then()
+                      .statusCode(200)
+                      .log().body()
+                      .extract().as(Observatory.class, raObjectMapper);
+
+      ProposalCycle newCycle = new ProposalCycle();
+      newCycle.setObservatory(lookHere);
+      newCycle.setTitle("Test observing cycle");
+      newCycle.setSubmissionDeadline(new Date());
+      newCycle.setObservationSessionStart(new Date());
+      newCycle.setObservationSessionEnd(new Date());
+
+      String body = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(newCycle);
+      System.out.println("New proposal cycle looks like this: " + body);
+      
+      given()
+              .when()
+              .body(body)
+              .contentType(JSON)
+              .post("proposalCycles/")
+              .then()
+              .statusCode(200)
+              .log().all();
+
+   }
+
+
    @Test
    void testGetCycles() {
       given()
