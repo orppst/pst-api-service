b8b59e4a4c68a646edfee48fa7585701389680c8
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-07-18 14:52:40 +0100

    try to force the repository management for snapshots

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,22 @@ pluginManagement {
     includeBuild("../build-logic")
 }
 
+//this does not always seem to be inherited?
+dependencyResolutionManagement {
+repositories {
+    mavenCentral()
+    mavenLocal()
+    /*
+    add this repository to pick up the SNAPSHOT version of the IVOA base library - in the future  this
+    will not be necessary when this library is released as a non-SNAPSHOT version.
+     */
+    maven {
+        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+    }
+    }
+}
+
+
 includeBuild("../pst-lib")
 
 rootProject.name="pst-api-service"
